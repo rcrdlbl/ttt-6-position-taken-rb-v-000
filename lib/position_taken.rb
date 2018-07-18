90dd106e7b1979c840500c6dd1a3598405e83ffd
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-18 19:45:57 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  if (board[index] != " ") || (board[index] != "")
-    return true
-  else
+  if (board[index] == " ") || (board[index] == "")
     return false
+  else
+    return true
   end
 
 end
